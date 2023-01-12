65f0a2fcccd56a71083fcc4c648b67feba0d25bd
polidex <lp.igor@gmail.com>
2023-01-12 16:24:07 -0300

    fix: reqt 03

--- app/backend/src/routes/login.router.ts
+++ app/backend/src/routes/login.router.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import requiredFields from '../middlewares/requiredFields';
 import hasUser from '../middlewares/hasUser';
-// import emailValidation from '../middlewares/emailValidation';
 import LoginController from '../controllers/login.controller';
 
 const loginRouter = Router();
 
 const loginController = new LoginController();
 
-loginRouter.post('/', hasUser, requiredFields, /* emailValidation, */ loginController.userLogin);
+loginRouter.post(
+  '/',
+  hasUser,
+  requiredFields,
+  loginController.userLogin,
+);
 
 export default loginRouter;
