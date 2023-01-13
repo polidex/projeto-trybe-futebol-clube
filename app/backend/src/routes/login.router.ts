a76ba7e5e926c71426723d69251aea994f64c2a2
polidex <lp.igor@gmail.com>
2023-01-13 14:45:36 -0300

    fix: na rota e apagando arquivo desnecessario

--- app/backend/src/routes/login.router.ts
+++ app/backend/src/routes/login.router.ts
@@ -1,17 +1,11 @@
 import { Router } from 'express';
 import requiredFields from '../middlewares/requiredFields';
-import hasUser from '../middlewares/hasUser';
 import LoginController from '../controllers/login.controller';
 
 const loginRouter = Router();
 
 const loginController = new LoginController();
 
-loginRouter.post(
-  '/',
-  hasUser,
-  requiredFields,
-  loginController.userLogin,
-);
+loginRouter.post('/', requiredFields, loginController.userLogin);
 
 export default loginRouter;
