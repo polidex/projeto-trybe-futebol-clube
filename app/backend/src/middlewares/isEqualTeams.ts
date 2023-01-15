0a496897fa2f6cb1f894cb94ea9777883379fafc
polidex <lp.igor@gmail.com>
2023-01-15 13:51:38 -0300

    feat: reqt 25

---
+++ app/backend/src/middlewares/isEqualTeams.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+const isEqualTeams = (req: Request, res: Response, next: NextFunction) => {
+  const { homeTeam, awayTeam } = req.body;
+
+  if (homeTeam === awayTeam) {
+    return res.status(422)
+      .json({ message: 'It is not possible to create a match with two equal teams' });
+  }
+  next();
+};
+
+export default isEqualTeams;
--- app/backend/src/routes/match.router.ts
+++ app/backend/src/routes/match.router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import isEqualTeams from '../middlewares/isEqualTeams';
 import MatchController from '../controllers/match.controller';
 
 const matchRouter = Router();
@@ -7,7 +8,7 @@ const matchController = new MatchController();
 
 matchRouter.get('/', matchController.getMatches);
 
-matchRouter.post('/', matchController.createMatch);
+matchRouter.post('/', isEqualTeams, matchController.createMatch);
 
 matchRouter.patch('/:id/finish', matchController.finishMatch);
 
